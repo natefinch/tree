b1e90b8481a048d587d6b999c3712317efbc9655
Nate Finch <nate.finch@gmail.com>
2013-02-27 08:42:21 -0500

    Changed the API of tree so that the tree and nodes are separate structures. This makes it possible for the tree to hold a comparison object so we don't have to pass in a comparison closure each time.

--- tree.go
+++ tree.go
@@ -5,47 +5,59 @@ package tree
 
 // Tree holds a binary tree data organization.
 //
-// Val is an index into an external data structure.
-//
 // Note that Tree is not intended to hold data itself, it just
-// maintains a structure, and data is retrieved use Val as the index
+// maintains a structure, and data is retrieved using a node's Val as the index
 // into another data structure that holds the actual values
 type Tree struct {
+	Head *Node
+	Cmp  Comparer
+}
+
+// Node is a node in the tree
+// Val is an index into an external data structure.
+type Node struct {
 	Val   int
-	Left  *Tree
-	Right *Tree
+	Left  *Node
+	Right *Node
+}
+
+// Comparer implements a comparison function that should return
+// -1 if data[i] < data[i], 0 if equal, and 1 if greater than
+type Comparer interface {
+	Compare(i, j int) int8
 }
 
 // New Returns a newly initialized Tree.
-func New() *Tree {
-	return &Tree{Val: -1}
+func New(cmp Comparer) *Tree {
+	return &Tree{Cmp: cmp}
 }
 
 // Insert adds the value to the tree.
 //
-// i is the key of the item in your datastructure
-//
-// cmp is a comparison function that should return
-// -1 if less than data[i], 0 if equal, and 1 if greater than
-func (t *Tree) Insert(i int, cmp func(int) int8) {
-	if t.Val == -1 {
-		t.Val = i
+// The value should already exist in the backing datastore at index i
+func (t *Tree) Insert(i int) {
+	if t.Head == nil {
+		t.Head = &Node{Val: i}
 		return
 	}
+	if t.Cmp == nil {
+		panic("Nil Comparer during Tree Insert")
+	}
+	n := t.Head
 	for {
-		switch cmp(t.Val) {
+		switch t.Cmp.Compare(i, n.Val) {
 		case -1:
-			if t.Left == nil {
-				t.Left = &Tree{Val: i}
+			if n.Left == nil {
+				n.Left = &Node{Val: i}
 				return
 			}
-			t = t.Left
+			n = n.Left
 		case 0, 1:
-			if t.Right == nil {
-				t.Right = &Tree{Val: i}
+			if n.Right == nil {
+				n.Right = &Node{Val: i}
 				return
 			}
-			t = t.Right
+			n = n.Right
 		default:
 			panic("Comparison function should only return 0, 1, or -1")
 		}
@@ -53,31 +65,20 @@ func (t *Tree) Insert(i int, cmp func(int) int8) {
 	panic("impossible")
 }
 
-// Find returns the index of the item if it is in the tree or -1 if it is not.
-//
-// cmp is a comparison function that should return
-// -1 if less than data[i], 0 if equal, and 1 if greater than
-func (t *Tree) Search(cmp func(int) int8) int {
-	t = t.search(cmp)
-	if t == nil {
-		return -1
+// Search returns the node of the tree with the value at index i if it exists
+func (t *Tree) Search(i int) *Node {
+	if t.Cmp == nil {
+		panic("Nil Comparer during search!")
 	}
-	return t.Val
-}
-
-func (t *Tree) search(cmp func(int) int8) *Tree {
-	if cmp == nil {
-		panic("Nil comparison function!")
-	}
-
-	for t != nil {
-		switch cmp(t.Val) {
+	n := t.Head
+	for n != nil {
+		switch t.Cmp.Compare(i, n.Val) {
 		case -1:
-			t = t.Left
+			n = n.Left
 		case 0:
-			return t
+			return n
 		case 1:
-			t = t.Right
+			n = n.Right
 		default:
 			panic("Comparison function should only return 0, 1, or -1")
 		}
@@ -86,12 +87,17 @@ func (t *Tree) search(cmp func(int) int8) *Tree {
 }
 
 // Walk implements an in-order walk of a tree using recursion.
-func Walk(t *Tree, f func(*Tree)) {
-	if t.Left != nil {
-		Walk(t.Left, f)
+func Walk(n *Node, f func(*Node) bool) bool {
+	if n.Left != nil {
+		if !Walk(n.Left, f) {
+			return false
+		}
 	}
-	f(t)
-	if t.Right != nil {
-		Walk(t.Right, f)
+	if !f(n) {
+		return false
 	}
+	if n.Right != nil {
+		return Walk(n.Right, f)
+	}
+	return true
 }
