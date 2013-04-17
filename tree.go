4c9c604ad9fe89ae5881512f52769097af6fc842
Nate <nate.finch@gmail.com>
2013-04-17 07:43:16 -0400

    clean up the api some, add delete method, compare is now a function that gets passed in at the site of the call.
    Note, this package now requires go 1.1

--- tree.go
+++ tree.go
@@ -3,6 +3,8 @@
 // in Go without using reflection or casts.
 package tree
 
+import "errors"
+
 // Tree holds a binary tree data organization.
 //
 // Note that Tree is not intended to hold data itself, it just
@@ -10,7 +12,6 @@ package tree
 // into another data structure that holds the actual values
 type Tree struct {
 	Head *Node
-	Cmp  Comparer
 }
 
 // Node is a node in the tree
@@ -21,83 +22,148 @@ type Node struct {
 	Right *Node
 }
 
-// Comparer implements a comparison function that should return
+// Comparer is a comparison function that should return
 // -1 if data[i] < data[i], 0 if equal, and 1 if greater than
-type Comparer interface {
-	Compare(i, j int) int8
-}
-
-// New Returns a newly initialized Tree.
-func New(cmp Comparer) *Tree {
-	return &Tree{Cmp: cmp}
-}
+type Compare func(i, j int) int8
 
 // Insert adds the value to the tree.
 //
 // The value should already exist in the backing datastore at index i
-func (t *Tree) Insert(i int) {
+// returns the node created in the tree and nil, or nil and and error if there was a problem
+func (t *Tree) Insert(i int, cmp Compare) (*Node, error) {
 	if t.Head == nil {
 		t.Head = &Node{Val: i}
-		return
+
+		return t.Head, nil
 	}
-	if t.Cmp == nil {
-		panic("Nil Comparer during Tree Insert")
+	if cmp == nil {
+		return nil, errors.New("Nil comparison")
 	}
 	n := t.Head
 	for {
-		switch t.Cmp.Compare(i, n.Val) {
+		switch cmp(i, n.Val) {
 		case -1:
 			if n.Left == nil {
 				n.Left = &Node{Val: i}
-				return
+				return n.Left, nil
 			}
 			n = n.Left
 		case 0, 1:
 			if n.Right == nil {
 				n.Right = &Node{Val: i}
-				return
+				return n.Right, nil
 			}
 			n = n.Right
 		default:
-			panic("Comparison function should only return 0, 1, or -1")
+			return nil, errors.New("Comparison function should only return 0, 1, or -1")
 		}
 	}
-	panic("impossible")
 }
 
-// Search returns the node of the tree with the value at index i if it exists
-func (t *Tree) Search(i int) *Node {
-	if t.Cmp == nil {
-		panic("Nil Comparer during search!")
+// Search returns the node of the tree with the value at index i if it exists, otherwise nil
+func (t *Tree) Search(i int, cmp Compare) (*Node, error) {
+	if cmp == nil {
+		return nil, errors.New("Nil Comparer!")
 	}
 	n := t.Head
 	for n != nil {
-		switch t.Cmp.Compare(i, n.Val) {
+		switch cmp(i, n.Val) {
 		case -1:
 			n = n.Left
 		case 0:
-			return n
+			return n, nil
 		case 1:
 			n = n.Right
 		default:
-			panic("Comparison function should only return 0, 1, or -1")
+			return nil, errors.New("Comparison function should only return 0, 1, or -1")
 		}
 	}
-	return nil
+	return nil, nil
+}
+
+// Delete removes the node of the tree with the value at index i
+//
+// Delete returns the index of rhte item if the value was found, otherwise will return an error
+func (t *Tree) Delete(i int, cmp Compare) (int, error) {
+	if cmp == nil {
+		return -1, errors.New("Nil Comparer!")
+	}
+
+	// parent holds the parent of the node we're deleting
+	var parent *Node
+
+	n := t.Head
+	for n != nil {
+		switch cmp(i, n.Val) {
+		default:
+			panic("Comparison function should only return 0, 1, or -1")
+		case -1:
+			parent = n
+			n = n.Left
+		case 1:
+			parent = n
+			n = n.Right
+		case 0:
+			val := n.Val
+			// found the node, let's delete it
+			if n.Left != nil {
+				right := n.Right
+				n.Val = n.Left.Val
+				n.Left = n.Left.Left
+				n.Right = n.Left.Right
+
+				// reinsert the values of the right subtree
+				if right != nil {
+					sub := &Tree{Head: n}
+					Walk(right, func(n *Node) bool {
+						sub.Insert(n.Val, cmp)
+						return true
+					})
+				}
+				return val, nil
+			}
+
+			if n.Right != nil {
+				// Left is nil, so we can just move Right up
+
+				n.Val = n.Right.Val
+				n.Left = n.Right.Left
+				n.Right = n.Right.Right
+				return val, nil
+			}
+
+			// deleting a leaf node, set parent pointer to nil
+
+			if parent == nil {
+				// deleting head
+				t.Head = nil
+				return val, nil
+			}
+
+			if parent.Left == n {
+				parent.Left = nil
+			} else {
+				parent.Right = nil
+			}
+			return val, nil
+		}
+	}
+	return -1, errors.New("Item not found")
 }
 
 // Walk implements an in-order walk of a tree using recursion.
+//
+// function f should return false if it wants the walk to stop
+// Walk returns false if f ever returns false, otherwise true
 func Walk(n *Node, f func(*Node) bool) bool {
-	if n.Left != nil {
-		if !Walk(n.Left, f) {
-			return false
-		}
+	if n == nil {
+		return true
+	}
+	if !Walk(n.Left, f) {
+		return false
 	}
 	if !f(n) {
 		return false
 	}
-	if n.Right != nil {
-		return Walk(n.Right, f)
-	}
-	return true
+	return Walk(n.Right, f)
 }
